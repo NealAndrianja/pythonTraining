db54ace6edf8bd79a4a00aa8f13831ffa76a4de7
NealAndrianja <mmcandrineal@gmail.com>
2025-01-29 10:18:44 +0100

    learning AI

--- list_Tuples_Dict.py
+++ list_Tuples_Dict.py
@@ -12,12 +12,17 @@ print("Fruits after removing banana:", fruits)
 for fruit in fruits:
     print("Fruit:", fruit)
 
-    # Step 1: Create a tuple of colors
-    colors = ("red", "green", "blue")
-    # Step 2: Access the second item in the tuple
-    print("Second color:", colors[1])
-    # Step 3: Try to change an item in the tuple (This will cause an error)
-    # colors[1] = "yellow" # Uncommenting this line will cause an error
-    # Step 4: Loop through each item in the tuple
-    for color in colors:
-        print("Color:", color)
+# Step 1: Create a tuple of colors
+colors = ("red", "green", "blue")
+# Step 2: Access the second item in the tuple
+print("Second color:", colors[1])
+# Step 3: Try to change an item in the tuple (This will cause an error)
+# colors[1] = "yellow" # Uncommenting this line will cause an error
+# Step 4: Loop through each item in the tuple
+for color in colors:
+    print("Color:", color)
+
+my_dict = {'a': 2}
+print(my_dict.get('b', 'Not_found'))
+
+
---
+++ methods and modules/Exercice1.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def print_factorial(n):
+    result = factorial(n)
+    print(f"Le factoriel est {result}")
+
+nbre = int(input("Entrer un nombre: "))
+print_factorial(nbre)
